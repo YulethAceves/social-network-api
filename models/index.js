fc2c97c8fcd2fbdb64ab6761f1c0bdb7778636d4
Yuleth Aceves <yulethaceves@gmail.com>
2023-07-27 12:20:46 -0600

    modified models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const Student = require('./Student');
-const Course = require('./Course');
+const Student = require('./thoughts');
+const Course = require('./user');
 
 module.exports = { Student, Course };
